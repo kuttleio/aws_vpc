3c87f98a954cccf97926f0091849cb393035532c
Dmitriy Solodukha <dspv@me.com>
2021-12-10 19:28:09 +0200

    Move from map() to tomap() (#2)

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ resource "aws_vpc" "vpc" {
   cidr_block           = var.vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
-  tags                 = merge(var.tags, map("Name", var.vpc_name))
+  tags                 = merge(var.tags, tomap({ Name = var.vpc_name }))
 }
 
 module "aws_internet_gateway" {
@@ -21,7 +21,7 @@ module "aws_internet_gateway" {
   // Apparently support will be added in 0.13 — https://github.com/hashicorp/terraform/issues/17519
   module_enabled = var.enable_internet_gateway == "true" ? "true" : "false"
   source         = "git@github.com:zbs-nu/source-launchpad-modules.git//aws_igw"
-  tags           = merge(var.tags, map("Name", "${var.vpc_name}-igw"))
+  tags           = merge(var.tags, tomap({ Name = "${var.vpc_name}-igw" }))
   vpc_id         = aws_vpc.vpc.id
 }
 
@@ -30,7 +30,7 @@ resource "aws_subnet" "public_subnets" {
   vpc_id            = aws_vpc.vpc.id
   cidr_block        = cidrsubnet(local.public_cidr, local.public_bit_diff, count.index)
   availability_zone = data.aws_availability_zones.available_az.names[count.index]
-  tags              = merge(var.tags, map("Name", "${var.vpc_name}-${var.enable_internet_gateway == "true" ? "public-${count.index + 1}" : "private-${count.index + 1 + (var.az_count * (var.enable_private_subnets == "true" ? 1 : 0))}"}"))
+  tags              = merge(var.tags, tomap({ Name = "${var.vpc_name}-${var.enable_internet_gateway == "true" ? "public-${count.index + 1}" : "private-${count.index + 1 + (var.az_count * (var.enable_private_subnets == "true" ? 1 : 0))}"}" }))
 }
 
 resource "aws_subnet" "private_subnets" {
@@ -38,13 +38,13 @@ resource "aws_subnet" "private_subnets" {
   vpc_id            = aws_vpc.vpc.id
   cidr_block        = cidrsubnet(local.private_cidr, local.private_bit_diff, count.index)
   availability_zone = data.aws_availability_zones.available_az.names[count.index]
-  tags              = merge(var.tags, map("Name", "${var.vpc_name}-private-${count.index + 1}"))
+  tags              = merge(var.tags, tomap({ Name = "${var.vpc_name}-private-${count.index + 1}" }))
 }
 
 resource "aws_route_table" "public_route" {
   count  = var.az_count * (var.enable_public_subnets == "true" ? 1 : 0)
   vpc_id = aws_vpc.vpc.id
-  tags   = merge(var.tags, map("Name", "${var.vpc_name}-${var.enable_internet_gateway == "true" ? "public-rt-${count.index + 1}" : "private-rt-${count.index + 1 + (var.az_count * (var.enable_private_subnets == "true" ? 1 : 0))}"}"))
+  tags   = merge(var.tags, tomap({ Name = "${var.vpc_name}-${var.enable_internet_gateway == "true" ? "public-rt-${count.index + 1}" : "private-rt-${count.index + 1 + (var.az_count * (var.enable_private_subnets == "true" ? 1 : 0))}"}" }))
 }
 
 resource "aws_route" "public_igw" {
@@ -63,7 +63,7 @@ resource "aws_route_table_association" "public_route_assoc" {
 resource "aws_route_table" "private_route" {
   count  = var.az_count * (var.enable_private_subnets == "true" ? 1 : 0)
   vpc_id = aws_vpc.vpc.id
-  tags   = merge(var.tags, map("Name", "${var.vpc_name}-private-rt-${count.index + 1}"))
+  tags   = merge(var.tags, tomap({ Name = "${var.vpc_name}-private-rt-${count.index + 1}" }))
 }
 
 resource "aws_route_table_association" "private_route_assoc" {
@@ -79,14 +79,14 @@ module "aws_eip" {
   module_enabled = (var.enable_nat_gateway == "true" ? "true" : "false")
   source         = "git@github.com:zbs-nu/source-launchpad-modules.git//aws_eip"
   eip_count      = (var.ha_nat_gateway == "true" ? var.az_count : 1) * (var.enable_public_subnets == "true" ? 1 : 0)
-  tags           = merge(var.tags, map("Name", "${var.vpc_name}-nat-eip"))
+  tags           = merge(var.tags, tomap({ Name = "${var.vpc_name}-nat-eip" }))
 }
 
 resource "aws_nat_gateway" "ngw" {
   count         = (var.ha_nat_gateway == "true" ? var.az_count : 1) * (var.enable_public_subnets == "true" ? 1 : 0) * (var.enable_nat_gateway == "true" ? 1 : 0)
   subnet_id     = element(aws_subnet.public_subnets.*.id, count.index)
   allocation_id = element(module.aws_eip.eip_alloc_id, count.index)
-  tags          = merge(var.tags, map("Name", "${var.vpc_name}-nat-${count.index + 1}"))
+  tags          = merge(var.tags, tomap({ Name = "${var.vpc_name}-nat-${count.index + 1}" }))
 }
 
 resource "aws_route" "private_nat_gateway" {
@@ -100,7 +100,7 @@ resource "aws_vpc_endpoint" "s3_endpoint" {
   count        = var.enable_private_subnets == "true" ? 1 : 0
   vpc_id       = aws_vpc.vpc.id
   service_name = "com.amazonaws.${data.aws_region.current.name}.s3"
-  tags         = merge(var.tags, map("Name", "${var.vpc_name}-endpoint-to-s3"))
+  tags         = merge(var.tags, tomap({ Name = "${var.vpc_name}-endpoint-to-s3" }))
 }
 
 resource "aws_vpc_endpoint_route_table_association" "s3_endpoint" {
@@ -113,7 +113,7 @@ resource "aws_vpn_gateway" "vpn_gw" {
   count           = var.enable_vpn_gateway == "true" ? 1 : 0
   vpc_id          = aws_vpc.vpc.id
   amazon_side_asn = var.vpn_gateway_amazon_asn
-  tags            = merge(var.tags, map("Name", "${var.vpc_name}-vgw"))
+  tags            = merge(var.tags, tomap({ Name = "${var.vpc_name}-vgw" }))
 }
 
 resource "aws_vpn_gateway_attachment" "default" {
